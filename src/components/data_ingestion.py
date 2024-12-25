c7356cab072bceb44bf687bffd4b22b203ef10d0
YashMasane <masaneyash6@gmail.com>
2024-12-25 23:40:59 +0530

    updated data ingestion

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 from dataclasses import dataclass
+from src.components.data_transformation import DataTransformer
 
 @dataclass
 class DataIngestioninfo:
@@ -43,4 +44,13 @@ class DataIngestion:
 
         except Exception as e:
             CustomException(e, sys)
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformer()
+    data_transformation.initialte_data_transformation(train_data, test_data)
+
 
